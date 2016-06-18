a706584bb6d0d0428bb3ac6b772ec65a703635a8
Vlad Pereskokov <pereskokow@gmail.com>
2016-06-18 17:33:21 +0300

    question cards

--- js/game-pension-app.js
+++ js/game-pension-app.js
@@ -1,9 +1,19 @@
 function Application () {
+  var self = this;
+
   this.user_age = 28;
 
   this.real_pension = 0;
 
   this.question_age_card = new QuestionAgeCard(this);
+
+  this.questions = [
+    "Auch im Alter wollen Sie einen gewissen Lebensstandard im Alltag und beim Wohnen erhalten. <br> Darf’s noch etwas mehr sein?",
+    "Auch im Alter wollen Sie einen gewissen Lebensstandard im Alltag und beim Wohnen erhalten. <br> Darf’s noch etwas mehr sein?",
+    "Auch im Alter wollen Sie einen gewissen Lebensstandard im Alltag und beim Wohnen erhalten. <br> Darf’s noch etwas mehr sein?",
+  ].map(function(question_text, index){
+    var q = new QuestionCard(self, index + 1, question_text);
+  });
 };
 
 Application.prototype.init = function() {
@@ -11,7 +21,38 @@ Application.prototype.init = function() {
 };
 
 
+/* QUESTION CARD */
 
+function QuestionCard ( app, question_number, question_text ) {
+  this.app = app;
+  this.num = question_number;
+  this.text = question_text;
+
+  this.is_transition = false;
+
+  this.$this = $("#Question_" + this.num);
+
+  this.$ = function(query) {
+    return this.$this.find(query);
+  }
+
+  this.slider = this.$( ".pension-question-slider" ).slider({
+    min: 1,
+    max: 5,
+    animate: false,
+    range: "min",
+    value: 1,
+    start: function( event, ui ) {
+      prev_value = ui.value;
+    },
+    stop: function( event, ui ) {
+      self.swapImages(prev_value, ui.value);
+    },
+    change: function( event, ui ) {
+      self.app.answers[ self.num - 1 ] = ui.value;
+    }
+  });
+};
 
 
 QuestionAgeCard = function(app) {
